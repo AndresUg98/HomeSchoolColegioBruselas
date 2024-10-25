eab93a82f716d483e7e42bd0c9a31e69ebe2c856
Andrés Ugarte <oscaran2109@gmail.com>
2024-10-24 20:22:24 -0600

    react router installed

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+// import { useRoutes } from 'react-router-dom'
 import Home from '../Home/index'
 import Login from '../Login/index'
 import Profile from '../Profile/index'
 import SubjectClass from '../SubjectClass/index'
 import VideoClass from '../VideoClass/index'
+
+import './App.css'
 function App() {
 
+  // let routes = useRoutes([
+  //   {
+  //     path: '/',
+  //     element: <Home/>
+  //   }
+  // ])
+  
   return (
     <>
       <Home/>
