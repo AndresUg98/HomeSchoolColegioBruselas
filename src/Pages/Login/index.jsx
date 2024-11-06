dae8e7ff1c73323fe39343045fb8c8343ff07c1d
Andrés Ugarte <oscaran2109@gmail.com>
2024-11-06 12:54:37 -0600

    adding logo to the login page and fixing some css

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,11 +1,15 @@
 import './login.css'
 import {Input} from '../../components/Input'
 import { Button } from '../../components/Button'
+import bruselasLogo from "../../assets/images/BruselasLogo.png"
 
 function Login() {
 
   return (
     <section className='login'>
+      <figure>
+        <img src={bruselasLogo} alt=""  />
+      </figure>
       <div className='input-container'>
         <Input placeholder="Correo electronico" type="text"/>
         <Input placeholder="Contraseña" type="password"/>
