7e12ccfb3b56dec5640e2c2253a4b80ee3d60bfa
Andrés Ugarte <oscaran2109@gmail.com>
2024-11-07 07:19:04 -0600

    adding the background circles

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -7,6 +7,11 @@ function Login() {
 
   return (
     <section className='login'>
+      {/* Circles background */}
+      <div className="circle circle1"></div>
+      <div className="circle circle2"></div>
+      <div className="circle circle3"></div>
+      <div className="circle circle4"></div>
       <figure>
         <img src={bruselasLogo} alt=""  />
       </figure>
