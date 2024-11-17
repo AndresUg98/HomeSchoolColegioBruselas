9157bf18055da497039e741a62e11a35be02c182
AndresUg98 <oscaran2109@gmail.com>
2024-11-17 17:20:07 -0600

    adjusting css and adding seach-bar layout

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,28 @@
 import "./Home.css";
 import { Card } from "../../components/Card";
+import { Input } from "../../components/Input";
 import aztecaImagen from "../../assets/images/aztecaImagen.jpg";
+import kameHouse from "../../assets/images/kameHouse.jpg";
+import bruselasLogo from "../../assets/images/BruselasLogo.png";
 
 function Home() {
   return (
     <>
+      <section className="logo-search-container">
+        <div className="logo-container">
+          <img src={bruselasLogo} alt="" />
+        </div>
+        <div className="serachBar-container">
+          <Input
+            css={"searchBar"}
+            type={"text"}
+            placeholder={"¿Que quieres aprender?"}
+          />
+        </div>
+      </section>
       <section className="banner-container">
         <Card
-          css={"banner"}
+          css={"general-card banner"}
           classImage={aztecaImagen}
           title={"Español 6° de primaria"}
         />
@@ -17,22 +32,22 @@ function Home() {
         <h2>Continuar aprendiendo</h2>
         <div className="carousel-container">
           <Card
-            css={"card"}
+            css={"general-card video-card"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"card"}
+            css={"general-card video-card"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"card"}
+            css={"general-card video-card"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"card"}
+            css={"general-card video-card"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
@@ -43,22 +58,22 @@ function Home() {
         <h2>Tus materias</h2>
         <div className="carousel-container">
           <Card
-            css={"card"}
+            css={"general-card video-card"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"card"}
+            css={"general-card video-card"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"card"}
+            css={"general-card video-card"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"card"}
+            css={"general-card video-card"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
@@ -69,9 +84,9 @@ function Home() {
         <h2>Frase del día</h2>
         <div className="carousel-container">
           <Card
-            css={"phrasecard"}
-            classImage={aztecaImagen}
-            title={"Español 6° de primaria"}
+            css={"general-card phrasecard"}
+            classImage={kameHouse}
+            title={"Siempre parece imposible hasta que se hace"}
           />
         </div>
       </section>
@@ -80,22 +95,22 @@ function Home() {
         <h2>Shorts</h2>
         <div className="carousel-container">
           <Card
-            css={"short"}
+            css={"general-card short"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"short"}
+            css={"general-card short"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"short"}
+            css={"general-card short"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
           <Card
-            css={"short"}
+            css={"general-card short"}
             classImage={aztecaImagen}
             title={"Español 6° de primaria"}
           />
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,8 @@
-import './Input.css'
+import "./Input.css";
 
 // eslint-disable-next-line react/prop-types
-function Input({placeholder, type}) {
-
-  return (
-    <input className='input' type={type} placeholder={placeholder} />
-  )
+function Input({ placeholder, type, css = "input" }) {
+  return <input className={css} type={type} placeholder={placeholder} />;
 }
 
-export  {Input}
+export { Input };
