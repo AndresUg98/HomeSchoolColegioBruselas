9c7ffc0107677e70969ce612279bcff27d3e027a
AndresUg98 <oscaran2109@gmail.com>
2024-11-17 14:36:03 -0600

    SHORTS section css added

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,45 +1,108 @@
-import './Home.css'
-import { Card } from '../../components/Card'
-import aztecaImagen from "../../assets/images/aztecaImagen.jpg"
+import "./Home.css";
+import { Card } from "../../components/Card";
+import aztecaImagen from "../../assets/images/aztecaImagen.jpg";
 
 function Home() {
-
   return (
     <>
       <section className="banner-container">
-          <Card css={"banner"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
+        <Card
+          css={"banner"}
+          classImage={aztecaImagen}
+          title={"Español 6° de primaria"}
+        />
       </section>
 
       <section className="section-container">
         <h2>Continuar aprendiendo</h2>
-        <div className='carousel-container'>
-        <Card css={"card"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
-        <Card css={"card"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
-        <Card css={"card"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
-        <Card css={"card"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
+        <div className="carousel-container">
+          <Card
+            css={"card"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"card"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"card"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"card"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
         </div>
-
       </section>
 
       <section className="section-container">
         <h2>Tus materias</h2>
-        <div className='carousel-container'>
-        <Card css={"card"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
-        <Card css={"card"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
-        <Card css={"card"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
-        <Card css={"card"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
+        <div className="carousel-container">
+          <Card
+            css={"card"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"card"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"card"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"card"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
         </div>
       </section>
 
       <section className="section-container">
         <h2>Frase del día</h2>
-        <div className='carousel-container'>
-        <Card css={"Phrasecard"} classImage={aztecaImagen} title={"Español 6° de primaria"}/>
+        <div className="carousel-container">
+          <Card
+            css={"phrasecard"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
         </div>
       </section>
 
+      <section className="section-container">
+        <h2>Shorts</h2>
+        <div className="carousel-container">
+          <Card
+            css={"short"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"short"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"short"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+          <Card
+            css={"short"}
+            classImage={aztecaImagen}
+            title={"Español 6° de primaria"}
+          />
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
